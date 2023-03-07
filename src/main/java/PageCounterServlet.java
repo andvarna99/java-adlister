37686b7bca0d62ae80a1b3ac3c54ff0fe493246d
Andrea Varnado <andrea.h.varnado@gmail.com>
2023-03-07 15:36:08 -0600

    3/7 exercises

---
+++ src/main/java/PageCounterServlet.java
@@ -0,0 +1,29 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "PageCounterServlet", urlPatterns = "/pagecounter")
+public class PageCounterServlet extends HttpServlet {
+    public int hitCount = 0;
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        hitCount++;
+
+
+//        if(Objects.equals(request.getParameter("resetcount"),"y")){
+//            hitCount= 0;
+//        }
+//
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+
+        out.println("<h1> Site visits count: "+ hitCount+"</h1>");
+
+
+
+    }
+}
