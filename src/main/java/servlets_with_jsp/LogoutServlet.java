09e8ba946addb2e51412fbc2bf0be756116e662a
Andrea Varnado <andrea.h.varnado@gmail.com>
2023-03-15 09:17:07 -0500

    logout servlet spacing

--- src/main/java/servlets_with_jsp/LogoutServlet.java
+++ src/main/java/servlets_with_jsp/LogoutServlet.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 public class LogoutServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         request.getSession().removeAttribute("user");
+        //clears everything out
         request.getSession().invalidate();
         response.sendRedirect("/login");
     }
